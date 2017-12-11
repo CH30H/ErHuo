cda10eb54d37f84a48990ea296d1c7c815bf3987
JiamingXie <jiaming.xie7@gmail.com>
2017-12-11 18:58:14 +0800

    frontend encrypted

--- js/LoginAndRegister.js
+++ js/LoginAndRegister.js
@@ -6,10 +6,18 @@ $('.tip a').click(function () {
 });
 
 $('#loginButton').click(function () {
+	var public_key = '-----BEGIN PUBLIC KEY-----\
+	MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC3//sR2tXw0wrC2DySx8vNGlqt\
+	3Y7ldU9+LBLI6e1KS5lfc5jlTGF7KBTSkCHBM3ouEHWqp1ZJ85iJe59aF5gIB2kl\
+	Bd6h4wrbbHA2XE1sq21ykja/Gqx7/IRia3zQfxGv/qEkyGOx+XALVoOlZqDwh76o\
+	2n1vP1D+tD3amHsK7QIDAQAB\
+	-----END PUBLIC KEY-----';
+	var key = RSA.getPublicKey(public_key);
+	var encrypted_passwd = RSA.encrypt($('#lpasswd').val(), key);
 	$.ajax({
 		type: 'post',
 		url: 'php/login.php',
-		data: {uid: $('#lemail').val(), passwd: $('#lpasswd').val()},
+		data: {uid: $('#lemail').val(), passwd: encrypted_passwd},
 		success: function(data) {
 			var obj = JSON.parse(data);
 			if (obj.status === 0) { alert("登录成功！"); }
@@ -23,10 +31,19 @@ $('#loginButton').click(function () {
 });
 
 $('#regButton').click(function () {
+	var public_key = '-----BEGIN PUBLIC KEY-----\
+	MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC3//sR2tXw0wrC2DySx8vNGlqt\
+	3Y7ldU9+LBLI6e1KS5lfc5jlTGF7KBTSkCHBM3ouEHWqp1ZJ85iJe59aF5gIB2kl\
+	Bd6h4wrbbHA2XE1sq21ykja/Gqx7/IRia3zQfxGv/qEkyGOx+XALVoOlZqDwh76o\
+	2n1vP1D+tD3amHsK7QIDAQAB\
+	-----END PUBLIC KEY-----';
+	var key = RSA.getPublicKey(public_key);
+	var encrypted_passwd = RSA.encrypt($('#rpasswd').val(), key);
+	var encrypted_wechatID = RSA.encrypt($('#rwechat').val(), key);
 	$.ajax({
 		type: 'post',
 		url: 'php/sign.php',
-		data: {uid: $('#remail').val(), passwd: $('#rpasswd').val(), nickname: $('#rnick').val(), wechatID: $('#rwechat').val()},
+		data: {uid: $('#remail').val(), passwd: encrypted_passwd, nickname: $('#rnick').val(), wechatID: encrypted_wechatID},
 		success: function(data) {
 			var obj = JSON.parse(data);
 			if (obj.status === 0) {
